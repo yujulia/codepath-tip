ab5a461911669fae0091335743c949325b7c200c
Julia Yu <juliayu@gmail.com>
2016-02-07 23:11:00 -0800

    add layout

--- devtips/ViewController.swift
+++ devtips/ViewController.swift
@@ -10,16 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billInput: UITextField!
+    @IBOutlet weak var tipOutput: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var total1: UILabel!
+    @IBOutlet weak var total2: UILabel!
+    @IBOutlet weak var total3: UILabel!
+    @IBOutlet weak var totalPanel: UIView!
+    @IBOutlet weak var tipPanel: UIView!
+    
+    func clearOutputs() {
+        tipOutput.text = ""
+        total1.text = ""
+        total2.text = ""
+        total3.text = ""
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        clearOutputs()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        billInput.becomeFirstResponder()
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditBegin(sender: AnyObject) {
+        
+    }
+
+    @IBAction func onEdit(sender: AnyObject) {
+        let tipPercent = [0.18, 0.2, 0.22]
+        let bill = NSString(string: billInput.text!).doubleValue
+        let tip = bill * tipPercent[tipControl.selectedSegmentIndex]
+        let total = tip + bill
+        let t2 = total/2
+        let t3 = total/3
+        
+        if (billInput.text == "") {
+            clearOutputs()
+        } else {
+
+        }
+        
+        tipOutput.text = String(format: "$%.2f", tip)
+        total1.text = String(format: "$%.2f", total)
+        total2.text = String(format: "$%.2f", t2)
+        total3.text = String(format: "$%.2f", t3)
+    }
+    
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 
 }
 
