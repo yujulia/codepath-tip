ad93d6658bb9c5686fbdaf763df48f8bb6248714
Julia Yu <juliayu@gmail.com>
2016-02-09 14:59:35 -0800

    save

--- devtips/SettingsViewController.swift
+++ devtips/SettingsViewController.swift
@@ -8,23 +8,52 @@
 
 import UIKit
 
+struct Style {
+    static func pinku() {
+        
+    }
+    
+    static func sax() {
+        
+    }
+}
+
 class SettingsViewController: UIViewController {
 
     @IBOutlet weak var PercentSegment: UISegmentedControl!
+    @IBOutlet weak var ThemeSegment: UISegmentedControl!
     
     let defaults = NSUserDefaults.standardUserDefaults()
     
-    override func viewWillAppear(animated: Bool) {
-        let defaultTip = defaults.integerForKey("defaultTip")
-        PercentSegment.selectedSegmentIndex = defaultTip
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // set default segments
+        
+        ThemeSegment.selectedSegmentIndex = 1
+        PercentSegment.selectedSegmentIndex = 1
+        
+        // if some segment has been choosen load those
+        
+        if (defaults.objectForKey("defaultTip") != nil) {
+            PercentSegment.selectedSegmentIndex = defaults.integerForKey("defaultTip")
+        }
+        
+        if (defaults.objectForKey("theme") != nil) {
+            ThemeSegment.selectedSegmentIndex = defaults.integerForKey("theme")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func onThemeChange(sender: UISegmentedControl) {
+        let theme = ThemeSegment.selectedSegmentIndex
+        
+        defaults.setObject(theme, forKey: "theme")
+        defaults.synchronize()
     }
     
     @IBAction func onPercentChange(sender: AnyObject) {
